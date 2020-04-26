9aaafbee242998369e0a6d1e87e129372ad5550a
Kamaldwip Choudhury (V) <kamaldwip_choudhury@apple.com>
2020-04-25 20:51:26 -0500

    Using Spring Environment Properties

--- src/main/java/com/kamal/springframework/dependencyInjection/kamalspringdi/config/PropertyConfig.java
+++ src/main/java/com/kamal/springframework/dependencyInjection/kamalspringdi/config/PropertyConfig.java
@@ -2,15 +2,20 @@ package com.kamal.springframework.dependencyInjection.kamalspringdi.config;
 
 
 import com.kamal.springframework.dependencyInjection.kamalspringdi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment environment;
 
     @Value("${kamal.database.username}")
     String username;
@@ -24,7 +29,8 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUserName(username);
+        //fakeDataSource.setUserName(username);
+        fakeDataSource.setUserName(environment.getProperty("KAMAL_USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setDbUrl(dbUrl);
         return fakeDataSource;
